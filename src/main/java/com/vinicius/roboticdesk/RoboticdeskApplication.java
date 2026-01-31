725dbc10c19872b7491fd1cc7d11676e5f6610be
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-01-31 18:01:57 -0300

    chore(init): add initial project structure

---
+++ src/main/java/com/vinicius/roboticdesk/RoboticdeskApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.roboticdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboticdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboticdeskApplication.class, args);
+	}
+
+}
